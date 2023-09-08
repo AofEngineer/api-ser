e1b46983e05efe11889d8079b134fdcf99d8c5d5
AofEngineer <124555693+AofEngineer@users.noreply.github.com>
2023-09-08 16:47:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var mysql = require('mysql2')
+require('dotenv').config()
+const pool = mysql.createPool({
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+})
+
+app.use(cors())
+
+app.get("/users",(req,res)=>{
+          pool.query("SELECT * FROM attractions",(req,rows)=>
+          res.json(rows)
+          )
+})
+
+app.listen(5000,()=>{
+          console.log('web server listening 5000')
+})
