7cee9431869856d927a26d485244a68c7dce304f
AofEngineer <124555693+AofEngineer@users.noreply.github.com>
2023-09-17 23:15:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-var mysql = require('mysql2')
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var mysql = require("mysql2");
+require("dotenv").config();
 const pool = mysql.createPool({
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_DATABASE,
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
-app.use(cors())
+app.use(cors());
 
-app.get("/users",(req,res)=>{
-          pool.query("SELECT * FROM attractions",(req,rows)=>
-          res.json(rows)
-          )
-})
-
-app.listen(5000,()=>{
-          console.log('web server listening 5000')
-})
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM attractions", (req, rows) => {
+ if (err) return res.status(500).json({ message: "Not excute" });
+    res.json(rows);
+  });
+});
+app.listen(5000, () => {
+  console.log("web server listening 5000");
+});
